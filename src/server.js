92d6270ebb508aff41493ae4402dfaa0eb2070fb
marshallford <inbox@marshallford.me>
2016-06-30 17:11:56 -0500

    CORS preflight

--- src/server.js
+++ src/server.js
@@ -7,10 +7,11 @@ import cors from 'cors' // Cross-origin resource sharing
 // define web server
 const app = express()
 app.server = http.createServer(app)
-app.disable('x-powered-by')
+app.disable('x-powered-by') // https://github.com/helmetjs/hide-powered-by
 app.use(bodyParser.json())
-app.use(cors())
-// define routes here, before catch-all functions
+app.use(cors()) // https://github.com/expressjs/cors#simple-usage-enable-all-cors-requests
+app.options('*', cors()) // https://github.com/expressjs/cors#enabling-cors-pre-flight
+// define routes here, before catch-all functions, after cors setup
 // app.use('/api/v1', api())
 app.use((req, res) => {
   res.status(404).json({ error: `cannot ${req.method} ${req.path}` })
@@ -23,4 +24,3 @@ app.use((err, req, res, next) => {
 // start web server
 app.server.listen(process.env.PORT || 8080)
 console.log(`Starting server: http://localhost:${app.server.address().port}`)
-
